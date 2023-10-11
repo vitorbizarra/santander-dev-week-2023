eac2fb8539ea1f6088d49bce8beb2a7edd18bb41
vitorbizarra <vitorbizarracontato@gmail.com>
2023-10-10 23:31:22 -0300

    :construction: Models

---
+++ src/main/java/me/dio/domain/model/User.java
@@ -0,0 +1,60 @@
+package me.dio.domain.model;
+
+import java.util.List;
+
+public class User {
+    private Long id;
+    private String name;
+    private Account acount;
+    private Card card;
+    private List<Feature> features;
+    private List<News> news;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAcount() {
+        return acount;
+    }
+
+    public void setAcount(Account acount) {
+        this.acount = acount;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+}
